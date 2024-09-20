b1b897e20ad845c1b9caacb329f6b6c2898b0dfb
Maedeh Khansari <khansari.maedeh@gmail.com>
2024-09-20 21:38:32 +0200

    add quiz code

---
+++ scripts/db.js
@@ -0,0 +1,82 @@
+let questions = [
+  {
+    "question": "Was ist das Standard-Dokumentformat für HTML?",
+    "answer_1": "XHTML",
+    "answer_2": "HTML4",
+    "answer_3": "HTML5",
+    "answer_4": "XML",
+    "right_answer": 3
+  },
+  {
+    "question": "Welche CSS-Eigenschaft wird verwendet, um die Textfarbe zu ändern?",
+    "answer_1": "color",
+    "answer_2": "background-color",
+    "answer_3": "font-size",
+    "answer_4": "text-align",
+    "right_answer": 1
+  },
+  {
+    "question": "Welches HTML-Tag wird verwendet, um eine Überschrift der höchsten Ebene zu erstellen?",
+    "answer_1": "<h6>",
+    "answer_2": "<title>",
+    "answer_3": "<h1>",
+    "answer_4": "<header>",
+    "right_answer": 3
+  },
+  {
+    "question": "Welche CSS-Eigenschaft steuert den Abstand innerhalb eines Elements?",
+    "answer_1": "margin",
+    "answer_2": "padding",
+    "answer_3": "border",
+    "answer_4": "width",
+    "right_answer": 2
+  },
+  {
+    "question": "Mit welchem HTML-Tag kann ein Bild eingefügt werden?",
+    "answer_1": "<img>",
+    "answer_2": "<picture>",
+    "answer_3": "<src>",
+    "answer_4": "<iframe>",
+    "right_answer": 1
+  },
+  {
+    "question": "Welches Attribut wird verwendet, um den Pfad eines Bildes in HTML anzugeben?",
+    "answer_1": "alt",
+    "answer_2": "src",
+    "answer_3": "href",
+    "answer_4": "link",
+    "right_answer": 2
+  },
+  {
+    "question": "Welche CSS-Eigenschaft ändert die Größe des Textes?",
+    "answer_1": "font-family",
+    "answer_2": "font-size",
+    "answer_3": "text-transform",
+    "answer_4": "letter-spacing",
+    "right_answer": 2
+  },
+  {
+    "question": "Welches HTML-Tag wird verwendet, um einen Hyperlink zu erstellen?",
+    "answer_1": "<a>",
+    "answer_2": "<link>",
+    "answer_3": "<nav>",
+    "answer_4": "<href>",
+    "right_answer": 1
+  },
+  {
+    "question": "Welche CSS-Eigenschaft wird verwendet, um den äußeren Abstand eines Elements zu steuern?",
+    "answer_1": "padding",
+    "answer_2": "border",
+    "answer_3": "margin",
+    "answer_4": "outline",
+    "right_answer": 3
+  },
+  {
+    "question": "Welches Attribut wird verwendet, um Text in einem Bild zu beschreiben, wenn das Bild nicht geladen werden kann?",
+    "answer_1": "alt",
+    "answer_2": "title",
+    "answer_3": "src",
+    "answer_4": "description",
+    "right_answer": 1
+  }  
+]
---
+++ scripts/script.js
@@ -0,0 +1,100 @@
+
+let rightQuestions = 0;
+let currentQuestion = 0;
+let AUDIO_SUCCESS = new Audio('audio/success.mp3');
+let AUDIO_FAIL = new Audio('audio/fail.mp3');
+
+function init() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+function showQuestion() {
+    if (gameIsOver()) {
+        showEndScreen();
+    } else {
+        updateToNextQuestion();
+    }
+    updateProgressBar();
+}
+
+function gameIsOver(){
+    return currentQuestion >= questions.length
+}
+
+function showEndScreen() {
+    //Show end Screen
+    document.getElementById('endScreen').style = '';
+    document.getElementById('questionBody').style = "display: none";
+    document.getElementById('amount-of-questions').innerHTML = questions.length;
+    document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
+    document.getElementById('header-image').src = 'img/win.png';
+}
+
+function updateProgressBar(){
+    let percent = currentQuestion / questions.length;
+    percent = Math.round(percent * 100);
+
+    document.getElementById('progress-bar').innerHTML = `${percent}%`;
+    document.getElementById('progress-bar').style = `width: ${percent}%`;
+}
+
+function updateToNextQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('question-number').innerHTML = currentQuestion + 1;
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerText = question['answer_1'];
+    document.getElementById('answer_2').innerText = question['answer_2'];
+    document.getElementById('answer_3').innerText = question['answer_3'];
+    document.getElementById('answer_4').innerText = question['answer_4'];
+}
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+
+    if (rightAnswerSelected(question, selectedQuestionNumber)) {
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+        AUDIO_SUCCESS.play();
+        rightQuestions++;
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById('answer_' + question['right_answer']).parentNode.classList.add('bg-success');
+        AUDIO_FAIL.play();
+    }
+    document.getElementById('next-button').disabled = false;
+}
+
+function rightAnswerSelected(question, selectedQuestionNumber){
+   return selectedQuestionNumber == question['right_answer'];
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButtons();
+    showQuestion();
+}
+
+function resetAnswerButtons() {
+    for (let i = 1; i < 5; i++) {
+        document.getElementById('answer_' + i).parentNode.classList.remove('bg-danger');
+        document.getElementById('answer_' + i).parentNode.classList.remove('bg-success');
+    }
+}
+
+function restartGame() {
+    document.getElementById('header-image').src = './img/backgraund.jpg';
+    document.getElementById('questionBody').style = '';
+    document.getElementById('endScreen').style = "display: none";
+    rightQuestions = 0;
+    currentQuestion = 0;
+    init();
+
+}
+
+
+
+
